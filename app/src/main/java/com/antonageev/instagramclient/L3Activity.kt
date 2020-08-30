52e3dfe9a8f4b7354a75f837f243fbcb68b55bbe
Anton Ageev <antforwork@mail.ru>
2020-08-30 23:59:12 +0300

    L3 buttons are ready

---
+++ app/src/main/java/com/antonageev/instagramclient/L3Activity.kt
@@ -0,0 +1,11 @@
+package com.antonageev.instagramclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class L3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_l3)
+    }
+}
